f1eca26d2fa74a2dbdb758fa60f1fad6806162b8
edjenal <edjenal.ferreira@gmail.com>
2012-12-12 18:13:26 -0300

    TOs criados

---
+++ regras_lanchonete/src/to/AreaFuncTO.java
@@ -0,0 +1,22 @@
+package to;
+
+public class AreaFuncTO {
+	
+	private int idArea;
+	private int idFunc;
+	
+	public int getIdArea() {
+		return idArea;
+	}
+	public void setIdArea(int idArea) {
+		this.idArea = idArea;
+	}
+	public int getIdFunc() {
+		return idFunc;
+	}
+	public void setIdFunc(int idFunc) {
+		this.idFunc = idFunc;
+	}
+	
+	
+}
---
+++ regras_lanchonete/src/to/TurnoFuncTO.java
@@ -0,0 +1,21 @@
+package to;
+
+public class TurnoFuncTO {
+	
+	private int idTurno;
+	private int idFunc;
+	
+	public int getIdTurno() {
+		return idTurno;
+	}
+	public void setIdTurno(int idTurno) {
+		this.idTurno = idTurno;
+	}
+	public int getIdFunc() {
+		return idFunc;
+	}
+	public void setIdFunc(int idFunc) {
+		this.idFunc = idFunc;
+	}
+	
+}
